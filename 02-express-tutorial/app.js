3c03fd80631943a47e19183a709d5941f0d15bc2
7vikram7 <vikramgore2007@gmail.com>
2021-06-20 20:31:45 +0530

    Serving all static content

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,28 +1,18 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).send('Home Page')
-})
+app.use(express.static('./public'))
 
-app.get('/about', (req, res) => {
-    res.status(200).send('About Page')
-})
+// app.get('/', (req, res) => {
+//     res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
 
 app.all('*', (req, res) => {
-    res.status(404).send('<h1> Oops incorrect address. Resource not found!</h1>')
+    res.status(404).send('Oops! Looks like you are lost. Resouce not found.')
 })
-
 
 app.listen(5000, () => {
-    console.log('server is listening on port 5000')
+    console.log('server is listening on port 5000....')
 })
-
-// app.get()
-// app.post()
-// app.put()
-// app.delete()
-// app.get()
-// app.get()
-// app.get()
-// app.get()
--- 02-express-tutorial/final/04-express-app.js
+++ 02-express-tutorial/final/04-express-app.js
@@ -3,17 +3,16 @@ const path = require('path')
 
 const app = express()
 
-// setup static and middleware
 app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+    res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
 
 app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
+    res.status(404).send('Oops! Looks like you are lost. Resouce not found.')
 })
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
+    console.log('server is listening on port 5000....')
 })
