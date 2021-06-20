89e3298206b34fbeb14a1eb526190b0c2e18f95d
7vikram7 <vikramgore2007@gmail.com>
2021-06-20 20:16:23 +0530

    Express basic example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,41 +1,28 @@
-const http = require('http')
-const {readFileSync} = require('fs')
+const express = require('express')
+const app = express()
 
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
-
-const server = http.createServer((req, res) => {
-    
-    const url = req.url 
-
-    if (url === '/') {
-        res.writeHead(200, {'content-type':'text/html'})
-        res.write(homePage)
-        res.end()
-    } else if (url === '/about') {
-        res.writeHead(200, {'content-type':'text/html'})
-        res.write('<h1>About Page Page</h1><p>This is a node express tutorial. Happy Learning!</p>')
-        res.end()
-    } else if (url === '/styles.css') {
-      res.writeHead(200, { 'content-type': 'text/css' })
-      res.write(homeStyles)
-      res.end()
-    } else if (url === '/logo.svg') {
-      res.writeHead(200, { 'content-type': 'image/svg+xml' })
-      res.write(homeImage)
-      res.end()
-    } else if (url === '/browser-app.js') {
-      res.writeHead(200, { 'content-type': 'text/javascript' })
-      res.write(homeLogic)
-      res.end()
-    } else {
-        res.writeHead(404, {'content-type':'text/html'})
-        res.write('<h1>Oops! Looks like you are lost.</h1>')
-        res.end()   
-     }
+app.get('/', (req, res) => {
+    res.status(200).send('Home Page')
 })
 
-server.listen(5000)
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page')
+})
 
+app.all('*', (req, res) => {
+    res.status(404).send('<h1> Oops incorrect address. Resource not found!</h1>')
+})
+
+
+app.listen(5000, () => {
+    console.log('server is listening on port 5000')
+})
+
+// app.get()
+// app.post()
+// app.put()
+// app.delete()
+// app.get()
+// app.get()
+// app.get()
+// app.get()
