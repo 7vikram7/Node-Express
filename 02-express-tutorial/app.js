24d57364f06548dfb664b08ca664cc8339206745
7vikram7 <vikramgore2007@gmail.com>
2021-06-21 06:25:53 +0530

    Basic Json

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,18 +1,11 @@
-const express = require('express')
-const path = require('path')
+const app = require('express')()
 
-const app = express()
+const {products} = require('./data')
 
-app.use(express.static('./public'))
-
-// app.get('/', (req, res) => {
-//     res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-// })
-
-app.all('*', (req, res) => {
-    res.status(404).send('Oops! Looks like you are lost. Resouce not found.')
+app.get('/', (req, res) => {
+    res.json(products)
 })
 
 app.listen(5000, () => {
-    console.log('server is listening on port 5000....')
+    console.log('Server is listening on port 5000')
 })
--- 02-express-tutorial/final/06-basic-json.js
+++ 02-express-tutorial/final/06-basic-json.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const app = express()
-const { products } = require('./data')
+const app = require('express')()
+
+const {products} = require('./data')
+
 app.get('/', (req, res) => {
-  res.json(products)
+    // res.json([{name: 'vikram'}, {name: 'vishakha'}])
+
+    res.json(products)
 })
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+    console.log('Server is listening on port 5000')
 })
