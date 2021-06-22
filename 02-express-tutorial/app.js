e03ca42e6c8b738f6eeeb91aaf2ee1c2e2ae3d02
7vikram7 <vikramgore2007@gmail.com>
2021-06-22 22:53:52 +0530

    middlewear basic

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,65 +1,23 @@
 const app = require('express')()
 
-const {products} = require('./data')
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url
+    const time = new Date().getFullYear()
+    console.log(method, url, time)
+    next()
+}
 
-app.get('/', (req, res) => {
-    // res.json([{name: 'vikram'}, {name: 'vishakha'}])
+app.get('/', logger, (req, res) => {
 
-    // res.json(products)
-
-    res.send('<h1> New Home Page</h1><a href="/api/products">products</a>')
+    res.send('Home')
 })
 
-app.get('/api/products', (req, res) => {
-    console.log('api/invoked')
-    const newProducts = products.map((product) => {
-        const {id, name, image} = product
-        return {id, name, image}
-    })
-    res.json(newProducts)
+app.get('/about', logger, (req, res) => {
+    res.send('About')
 })
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-    res.send('Random hardcoded review')
-})
-
-app.get('/api/products/:productID', (req, res) => {
-    console.log(req)
-    console.log(req.params)
-    const {productID} = req.params
-    const requestedProduct = products.find((product) => {
-        if (product.id === Number(productID)) {
-            return true
-        }
-        return false
-    })
-
-    if(!requestedProduct) {
-        res.status(404).send('Product Does not exist')
-    }
-
-    res.json(requestedProduct)
-})
-
-app.get('/api/v1/query', (req, res) => {
-    console.log(req.query)
-
-    const {search, limit} = req.query
-    let sortedProducts = [...products]
-
-    if (search) {
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search)
-        })
-    }
-
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0,limit)
-    }
-  
-    res.status(200).json(sortedProducts)
-  })
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000')
 })
+
