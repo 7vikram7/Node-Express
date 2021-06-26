fe848db8baada9d9b6f31c24088838f5de7bbebd
7vikram7 <vikramgore2007@gmail.com>
2021-06-26 23:40:35 +0530

    Put Request (WIP)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,7 +7,7 @@ let {people} = require('./data')
 app.use(express.static('./methods-public'))
 
 //parse form data
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:true}))
 
 app.get('/api/people', (req, res) => {
     res.status(200).json({success:true, data: people})
@@ -23,6 +23,14 @@ app.post('/login', (req, res) => {
         return res.status(200).send(`Welcome ${name}`)
     }
     res.status(401).send('Please Provide Credentials')
+})
+
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+    console.log(id, name)
+    console.log(req.body)
+    res.send('hello world')
 })
 
 app.listen(5000, () => {
