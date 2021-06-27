720ca8a34dd386fea7195a9678ebe1b7f5a4d03e
7vikram7 <vikramgore2007@gmail.com>
2021-06-27 21:57:48 +0530

    Delete method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,13 +1,21 @@
 const app = require('express')()
 const express = require('express')
+var bodyParser = require('body-parser');
 
-let {people} = require('./data')
+let {people} = require('./data');
+const e = require('express');
 
 // static assets
 app.use(express.static('./methods-public'))
 
 //parse form data
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:false}))
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 
 app.get('/api/people', (req, res) => {
     res.status(200).json({success:true, data: people})
@@ -31,6 +39,20 @@ app.put('/api/people/:id', (req, res) => {
     console.log(id, name)
     console.log(req.body)
     res.send('hello world')
+})
+
+app.delete('/api/people/:id', (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+    if (!person) {
+        return res
+        .status(404)
+        .json({succss: false, msg: `no person with id ${req.params.id}`})
+    }
+
+    const newPeople = people.filter((person) => person.id != req.params.id)
+    return res
+    .status(200)
+    .json({succss: true, data: newPeople})
 })
 
 app.listen(5000, () => {
